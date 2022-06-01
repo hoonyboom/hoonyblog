171cfd6bdd725de46f0dc0fddf5a74dfa6d64484
hyezo <hyezoprk@kakao.com>
2022-06-01 22:58:34 +0900

    etc: Next 튜토리얼

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,15 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
-
-export default MyDocument;
