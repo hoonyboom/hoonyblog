1d36a87454c7442b66ea4b6eb8b05c9ede446bf2
hyezo <hyezoprk@kakao.com>
2022-06-06 17:56:41 +0900

    fix: 경로 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Date from '@/components/date';
-import Layout, { siteTitle } from '@/components/layout';
-import { getSortedPostsData } from '@/lib/posts';
+import { Layout, Date } from '@/components';
+import { siteTitle } from '@/components/layout';
+import { getSortedPostsData } from '@/lib/posts.js';
 import utilStyles from '@/styles/utils.module.css';
 
 
