e98b99b3e49cedf220a428199a740ebd92874d0e
hyezo <hyezoprk@kakao.com>
2022-06-06 17:23:25 +0900

    init: tailwind 테마 셋팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '465px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1640px',
+    },
+    fontSize: {
+      // Set in Major Third typescale (1.25)
+      base: '1em',
+      lg: '1.25em',
+      xl: '1.563em',
+      '2xl': '1.953em',
+      '3xl': '2.441em',
+      '4xl': '3.052em',
+      '5xl': '3.815em',
+    },
   },
   plugins: [],
 }
