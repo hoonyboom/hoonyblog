4b2775687f762a1ac83fa02af18c77f2cd6ae280
hyezo <hyezoprk@kakao.com>
2022-06-06 23:38:56 +0900

    refactor: mdx 셋팅

---
+++ src/lib/posts.ts
@@ -0,0 +1,76 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { bundleMDX } from 'mdx-bundler';
+import remarkGfm from 'remark-gfm';
+import rehypePrism from 'rehype-prism/lib/src';
+
+interface meta {
+  [key: string]: string
+}
+
+// const blogDirectory = path.join(process.cwd(), "blog")
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.mdx$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort(({ date: a }: meta, { date: b }: meta) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.mdx$/, ''),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.mdx`);
+  const source = fs.readFileSync(fullPath, 'utf8');
+
+  const { code, frontmatter } = await bundleMDX({source: source, 
+    mdxOptions(options) {
+      options.remarkPlugins = [...(options?.remarkPlugins ?? []), remarkGfm],
+      options.rehypePlugins = [...(options?.rehypePlugins ?? []), rehypePrism]
+      return options;
+    },
+  });
+
+  return {
+    id,
+    code,
+    frontmatter,
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { Layout, Date } from '@/components';
 import { siteTitle } from '@/components/layout';
-import { getSortedPostsData } from '@/lib/posts.js';
+import { getSortedPostsData } from '@/lib/posts';
 import utilStyles from '@/styles/utils.module.css';
 
 
@@ -44,6 +44,7 @@ export default function Home({ allPostsData }: PostsProps) {
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small>
+              <h1 className="text-xl">바보</h1>
             </li>          ))}
         </ul>
       </section>
