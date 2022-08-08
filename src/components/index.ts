a175453748d206a6623c73bdf575c1109d30bad8
ㄱㅎ <98998082+10004ok@users.noreply.github.com>
2022-08-08 23:02:03 +0900

    에러

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Layout } from "./layout";
-export { default as Date } from "./date";
-export { default as Nav } from "./nav";
-export { default as HomeNav } from "./homeNav";
-export { default as Darkmode } from "./darkmode";
-export { default as CommandPalette } from "./commandPalette";
-export { default as CommandPaletteIcon } from "./commandIcon";
-export { default as Logo } from "./logo";
-// export { default as MdxComponent } from "./MdxComponent";
