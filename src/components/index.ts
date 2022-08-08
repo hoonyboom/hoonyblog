79739d33e5944d61e3ad0dcddb5b78c699de3d83
hyezo <hyezoprk@kakao.com>
2022-08-08 23:11:05 +0900

    f

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
-export { default as Layout } from "./layout";
-export { default as Date } from "./date";
-export { default as Nav } from "./nav";
-export { default as HomeNav } from "./homeNav";
-export { default as Darkmode } from "./darkmode";
-export { default as CommandPalette } from "./commandPalette";
-export { default as CommandPaletteIcon } from "./commandIcon";
-export { default as Logo } from "./logo";
+export { default as Layout } from "./Layout";
+export { default as Date } from "./Date";
+export { default as Nav } from "./Nav";
+export { default as HomeNav } from "./HomeNav";
+export { default as Darkmode } from "./Darkmode";
+export { default as CommandPalette } from "./CommandPalette";
+export { default as CommandPaletteIcon } from "./CommandIcon";
+export { default as Logo } from "./Logo";
 // export { default as MdxComponent } from "./MdxComponent";
