26d23bac3599121ae040f8075a0ce2252390167f
hyezo <hyezoprk@kakao.com>
2022-08-08 22:56:47 +0900

    fix: 파일명 소문자로 바꿈

---
+++ src/components/homeNav.tsx
@@ -0,0 +1,10 @@
+import { Darkmode, CommandPaletteIcon } from "@/components";
+
+export default function Nav() {
+  return (
+    <div className="flex justify-end my-8 space-x-3 sm:mr-4">
+      <CommandPaletteIcon  />
+      <Darkmode />
+    </div>
+  );
+}
