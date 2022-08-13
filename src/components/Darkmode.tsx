a17f2e9acba60a3a84d936442a7d1411dd526b09
hyezo <hyezoprk@kakao.com>
2022-08-14 08:29:27 +0900

    fix: prop "d" pre-render problem on darkmode

--- src/components/Darkmode.tsx
+++ src/components/Darkmode.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 import useSound from "use-sound";
@@ -7,17 +7,24 @@ export default function Darkmode() {
   const { theme, setTheme } = useTheme();
   const [rotate, setRotate] = useState(false);
   const [switch2] = useSound("/sounds/switch.mp3");
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    setRotate(true);
+  }, []);
 
   return (
-    <button className={`md:inline hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-900 dark:hover:text-gray-100 rounded-md
-    transition-transform origin-center duration-300
-          ${rotate ? "rotate-0" : "rotate-45"}
-    `}>
-      {theme === "dark" ? (
+    <button
+      className={`md:inline hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-900 dark:hover:text-gray-100 rounded-md
+      transition-transform origin-center duration-300
+            ${rotate ? "rotate-0" : "rotate-45"}
+      `}>
+      {mounted && theme === "dark" ? (
         <MdDarkMode
           onMouseUp={() => switch2()}
           onClick={() => {
-            setTheme(theme === "dark" ? "light" : "dark");
+            setTheme("light");
             setRotate(false);
           }}
           className={`w-6 h-6 `}
@@ -27,7 +34,7 @@ export default function Darkmode() {
         <MdOutlineLightMode
           onMouseUp={() => switch2()}
           onClick={() => {
-            setTheme(theme === "dark" ? "light" : "dark");
+            setTheme("dark");
             setRotate(true);
           }}
           className={`w-6 h-6`}
