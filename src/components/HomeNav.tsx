7f57678fe90c7572618ca0a22f12c91a81522e3f
hyezo <hyezoprk@kakao.com>
2022-08-16 22:12:16 +0900

    feat: Layout error fix

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -2,7 +2,7 @@ import { Darkmode, CommandPaletteIcon } from "@/components";
 
 export default function Nav() {
   return (
-    <div className="my-8 mr-4 flex justify-end space-x-3 dark:bg-zinc-900/90">
+    <div className="flex justify-end space-x-3 py-8 pr-4 dark:bg-zinc-900/90">
       <CommandPaletteIcon />
       <Darkmode />
     </div>
