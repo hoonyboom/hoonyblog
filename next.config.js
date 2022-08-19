cb5edc3d89d9888a3e417fd0e5bfa1587c892588
hyezo <hyezoprk@kakao.com>
2022-08-20 00:28:04 +0900

    hass css

--- next.config.js
+++ next.config.js
@@ -30,4 +30,22 @@ module.exports = {
   //     },
   //   ];
   // },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules[3].oneOf.forEach((moduleLoader, i) => {
+      Array.isArray(moduleLoader.use) &&
+        moduleLoader.use.forEach(l => {
+          if (l.loader.includes("\\css-loader") && !l.loader.includes("postcss-loader")) {
+            const { getLocalIdent, ...others } = l.options.modules;
+            l.options = {
+              ...l.options,
+              modules: {
+                ...others,
+                localIdentName: "[hash:base64:6]",
+              },
+            };
+          }
+        });
+    });
+    return config;
+  },
 };
