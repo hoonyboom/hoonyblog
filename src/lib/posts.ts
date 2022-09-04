f8c9a0f1a12cab0470e8e7e0f880e1e6224ad585
hyezo <hyezoprk@kakao.com>
2022-09-04 19:17:53 +0900

    edited

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -5,7 +5,7 @@ import { bundleMDX } from "mdx-bundler";
 import { remarkCodeHike } from "@code-hike/mdx";
 import remarkGfm from "remark-gfm";
 import rehypePrism from "rehype-prism/lib/src";
-import { theme } from "./search-light.js";
+import { theme } from "./search-light";
 
 interface dateType {
   [key: string]: string;
