0d88e788d5fda3fb31dfd8c90e653baf33f4d55c
hyezo <hyezoprk@kakao.com>
2022-09-12 02:45:28 +0900

    ...

--- next.config.js
+++ next.config.js
@@ -4,21 +4,21 @@ module.exports = {
   experimental: { images: { allowFutureImage: true } },
   async redirects() {
     return [
-      // {
-      //   source: "/coding",
-      //   destination: "/",
-      //   permanent: false,
-      // },
-      // {
-      //   source: "/writing",
-      //   destination: "/",
-      //   permanent: false,
-      // },
-      // {
-      //   source: "/about",
-      //   destination: "/category/about",
-      //   permanent: false,
-      // },
+      {
+        source: "/coding",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/writing",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/about",
+        destination: "/category/about",
+        permanent: false,
+      },
     ];
   },
   async rewrites() {
