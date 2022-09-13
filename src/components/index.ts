cc51596c312fe3e826ddd4524d1f49c9f916cf6b
hyezo <hyezoprk@kakao.com>
2022-09-13 14:24:26 +0900

    쓰임별로 구분(임시)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { default as Seo } from "./Seo";
+export { default as Logo } from "./Logo";
 export { default as Layout } from "./Layout";
 export { default as Date } from "./Date";
 export { default as Nav } from "./Nav";
@@ -5,10 +7,9 @@ export { default as HomeNav } from "./HomeNav";
 export { default as Darkmode } from "./Darkmode";
 export { default as CommandPalette } from "./CommandPalette";
 export { default as CommandPaletteIcon } from "./CommandIcon";
-export { default as Logo } from "./Logo";
 export { default as MdxComponents } from "./MdxComponents";
+
 export { default as Pagination } from "./Pagination";
-export { default as Seo } from "./Seo";
 export { default as Profile } from "./Profile";
 export { default as CategoryTabs } from "./CategoryTabs";
 export { default as TabTracker } from "./TabTracker";
