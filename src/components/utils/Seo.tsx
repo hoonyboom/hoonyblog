8f896bd6c80be56d0db5a4494636bad033dfb807
hyezo <hyezoprk@kakao.com>
2022-09-22 20:28:54 +0900

    feat: utterance 댓글창

--- src/components/utils/Seo.tsx
+++ src/components/utils/Seo.tsx
@@ -7,7 +7,7 @@ export default function Seo({ siteTitle }: { siteTitle?: string }) {
       <link rel="icon" href="/images/2022/summer/heart.svg" />
       <link rel="apple-touch-icon" href="/images/2022/summer/heart.svg" />
       <link rel="mask-icon" href="/images/2022/summer/heart.svg" />
-      <link rel="canonical" href="https://hyezoprk.com/" />,
+      <link rel="canonical" href="https://www.hyezoprk.com" />,
       <meta name="description" content="프론트엔드 취준생 블로그" />
       <meta
         property="og:image"
@@ -16,7 +16,7 @@ export default function Seo({ siteTitle }: { siteTitle?: string }) {
         )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
       />
       <meta property="og:title" content={siteTitle} />
-      <meta property="og:url" content="https://chorok.vercel.app" />
+      <meta property="og:url" content="https://www.hyezoprk.com" />
       <meta property="og:locale" content="ko_KR" />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
