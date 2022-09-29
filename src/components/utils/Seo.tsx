bda7c1c0bd703845000f950ac8901f2dac75a182
hyezo <hyezoprk@kakao.com>
2022-09-29 16:45:03 +0900

    edit: 포스트 내용 추가 및 수정

--- src/components/utils/Seo.tsx
+++ src/components/utils/Seo.tsx
@@ -7,14 +7,7 @@ export default function Seo({ siteTitle }: { siteTitle?: string }) {
       <link rel="icon" href="/images/2022/summer/heart.svg" />
       <link rel="apple-touch-icon" href="/images/2022/summer/heart.svg" />
       <link rel="mask-icon" href="/images/2022/summer/heart.svg" />
-      <link rel="canonical" href="https://www.hyezoprk.com" />,
-      <meta name="description" content="프론트엔드 취준생 블로그" />
-      <meta
-        property="og:image"
-        content={`https://og-image.vercel.app/${encodeURI(
-          "혜조로그",
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-      />
+      <link rel="canonical" href="https://www.hyezoprk.com" />
       <meta property="og:title" content="혜조로그" />
       <meta property="og:description" content="코딩과 일기가 뒤죽박죽 섞여 있어요" />
       <meta property="og:image" content="/images/2022/summer/heart.svg" />
@@ -23,6 +16,13 @@ export default function Seo({ siteTitle }: { siteTitle?: string }) {
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="robots" content="all" />
+      <meta name="description" content="프론트엔드 취준생 블로그" />
+      {/* <meta
+        property="og:image"
+        content={`https://og-image.vercel.app/${encodeURI(
+          "혜조로그",
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+      /> */}
     </Head>
   );
 }
