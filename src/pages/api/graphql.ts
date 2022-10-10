2fc111b65a3d74167dd000f936e73482296c5287
hyezo <hyezoprk@kakao.com>
2022-10-10 13:48:19 +0900

    🐛fix: test,,

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -23,16 +23,20 @@ const apolloServer = new ApolloServer({
 const startServer = apolloServer.start();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers",
-  // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD",
-  // );
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "https://hyezoprk.com");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers",
+    );
+    // res.end();
+    return false;
+  }
 
   await startServer;
   await apolloServer.createHandler({
