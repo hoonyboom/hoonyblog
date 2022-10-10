f9d79e5dc8a12187ca4403e0ad41330db497db2f
hyezo <hyezoprk@kakao.com>
2022-10-10 14:25:35 +0900

    🐛fix: ef

--- src/lib/graphql/apollo-client.ts
+++ src/lib/graphql/apollo-client.ts
@@ -1,11 +1,12 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: `${
-    process.env.NODE_ENV === "production"
-      ? "https://hyezoprk.com/api/graphql"
-      : "http://localhost:3000/api/graphql"
-  }`,
+  // uri: `${
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://hyezoprk.com/api/graphql"
+  //     : "http://localhost:3000/api/graphql"
+  // }`,
+  uri: "https://hyezoprk.com/api/graphql",
   credentials: "include",
 });
 
