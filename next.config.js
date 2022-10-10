869bf545df4d8ffd0130360e71d0883151eca30f
hyezo <hyezoprk@kakao.com>
2022-10-10 12:30:31 +0900

    🐛fix: test again

--- next.config.js
+++ next.config.js
@@ -19,15 +19,35 @@ module.exports = {
       },
     ];
   },
-  async rewrites() {
+  // async rewrites() {
+  //   return [
+  //     // {
+  //     //   source: "/api/graphql",
+  //     //   destination: "https://hyezoprk.com",
+  //     // },
+  //     {
+  //       source: "/api/graphql",
+  //       destination: "https://studio.apollographql.com",
+  //     },
+  //   ];
+  // },
+  async headers() {
     return [
-      // {
-      //   source: "/api/graphql",
-      //   destination: "https://hyezoprk.com",
-      // },
       {
-        source: "/api/graphql",
-        destination: "https://studio.apollographql.com",
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -23,19 +23,19 @@ const apolloServer = new ApolloServer({
 const startServer = apolloServer.start();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers",
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD",
-  );
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers",
+  // );
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD",
+  // );
 
   if (req.method === "OPTIONS") {
-    res.end();
+    res.status(200).end();
     return false;
   }
 
