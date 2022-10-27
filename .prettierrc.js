6a5bbb972590a189a5111613ef08dc2bfd3335ef
hyezo <hyezoprk@kakao.com>
2022-10-27 17:49:21 +0900

    📝docs(new post added): n
    n

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,6 @@ module.exports = {
   tabWidth: 2,
   printWidth: 90,
   trailingComma: "all",
-  bracketSameLine: false, // jsx에서  /> 를 줄바꿈으로 내림
-  arrowParens: "avoid", // 화살표 함수 괄호 생략될 시 생략
+  bracketSameLine: false, // false: jsx에서  /> 를 줄바꿈으로 내림
+  arrowParens: "avoid", // avoid: 화살표 함수 괄호 생략될 시 생략
 };
