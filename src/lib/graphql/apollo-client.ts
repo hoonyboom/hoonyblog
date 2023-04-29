cad509ca6b51db39599f511453b346d5b96109e2
hyezo <hyezoprk@kakao.com>
2023-04-29 14:58:10 +0900

    🐛 fix: apollo uri is modified

--- src/lib/graphql/apollo-client.ts
+++ src/lib/graphql/apollo-client.ts
@@ -1,11 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: `${
-    process.env.NODE_ENV === "production"
-      ? "https://hyezoblog.vercel.app/api/graphql"
-      : "http://localhost:3000/api/graphql"
-  }`,
+  uri: "/api/graphql",
   headers: {
     "Content-Type": "application/json",
   },
