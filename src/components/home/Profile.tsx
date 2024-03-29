8eaa01703c6a9da25f05686e456844b7234c598d
hoonyboom <hyezoprk@kakao.com>
2024-03-29 11:20:35 +0900

    블로그명 변경

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -16,7 +16,7 @@ export default function Profile({ initCategory }: { initCategory: boolean }) {
             alt="프로필"
           />
           <div className="ml-8 flex flex-col justify-center space-y-1">
-            <p className="pt-0 font-heading text-lg">Hyezolog</p>
+            <p className="pt-0 font-heading text-lg">hoonylog</p>
             <p className="pt-0 text-base">공부일지</p>
           </div>
         </section>
